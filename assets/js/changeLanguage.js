9f5e083ed799d9c7409dacd3bed4ff54b4bad082
Carlos <carlostorres2912@yahoo.com.com>
2023-12-05 23:49:14 -0500

    advances

--- assets/js/changeLanguage.js
+++ assets/js/changeLanguage.js
@@ -14,14 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const translation = phrase.getAttribute('data-translate-en');
         phrase.textContent = translation
       }
-       document.getElementById('downloadCV').href = '../cv/cv_en.png' 
+       document.getElementById('downloadCV').href = '../../assets/cv/cv_en.png' 
       language = 'en'
     } else {
       for (const phrase of elementsToTransalate) {
         const translation = phrase.getAttribute('data-translate-es');
         phrase.textContent = translation
       }
-       document.getElementById('downloadCV').href = '../cv/cv_es.png' 
+       document.getElementById('downloadCV').href = '../../assets/cv/cv_es.png' 
       language = 'es'
 
     }
